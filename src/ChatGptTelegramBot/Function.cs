b505fd9d51e81df17c229123bdcfda367d6ce3af
KonH <konh@yandex.ru>
2023-07-17 19:36:25 +0200

    Basic project structure

---
+++ src/ChatGptTelegramBot/Function.cs
@@ -0,0 +1,14 @@
+using System;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ChatGptTelegramBot;
+
+public class Function {
+
+    public string FunctionHandler(string input, ILambdaContext context) {
+        return input.ToUpper();
+    }
+}
